9684d37c3a647eb8f5631cd5b3965a2a0a6c8270
BilgeKaanCansu <147918686+BilgeKaanCansu@users.noreply.github.com>
2024-06-09 12:07:42 +0300

    Prime Numbers Question

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	bool flag;
+	for (int i = 500; i<600; i++) {
+		flag = true;
+		for (int k = 2; k<i/2; k++) {
+			if (i % k == 0) {
+				flag = false;
+				break;
+			}
+		}
+		if (flag) cout << i<<" ";
+ }
+	return 0;
+}
